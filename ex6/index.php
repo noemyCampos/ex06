350c1a0ba538f168af392dad42aee42b2eb0b3c4
noemyCampos <141765234+noemyCampos@users.noreply.github.com>
2023-11-14 14:32:44 -0400

    Add files via upload

---
+++ ex6/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $valor1 = $_GET['v1'] ??0;
+    $valor2 = $_GET['v2'] ??0;
+    ?>
+    <main>
+        <h1>Somador de Valor</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
+        <lebel for="v1">Valor 1</lebel>
+        <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+        <lebel for="v2">Valor 2</lebel>
+        <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+        <input type="submit" id="v2" value="somar">
+        </form>
+    </main>
+    <section id="resultado" >
+        <h2>Resultado da Soma</h2>
+        <?php
+        $soma = $valor1 + $valor2;
+        echo "<p>A soma entre os valores <strong>$valor1</strong>
+        </strong> é igual a <strong>$soma</strong.</p>"
+        ?>
+</body>
+</html>
